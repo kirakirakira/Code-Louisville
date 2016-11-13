bf19392aa2af361474124d02b8e2135ee25b2c46
Kira Hartlage <kira.hartlage@gmail.com>
2016-11-13 15:29:04 -0500

    finished video 3

--- JS-loops-arrays-objects/Section_1_ Loops/video03/app.js
+++ JS-loops-arrays-objects/Section_1_ Loops/video03/app.js
@@ -1,3 +1,16 @@
+var upper = 10000;
+var randomNum = randomNumber(upper);
+var guess;
+var attempts = 0;
+
 function randomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+
+while(guess != randomNum) {
+    guess = randomNumber(upper);
+    attempts+=1;
+}
+
+document.write("<p>The random number was: " + randomNum + "</p>");
+document.write("<p>It took the computer " + attempts + " guesses to get the correct number.</p>");
