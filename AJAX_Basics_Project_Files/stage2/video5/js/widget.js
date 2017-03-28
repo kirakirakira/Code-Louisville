91b974397c420d9a4ed49a83a8437fb58e6ed3ea
Kira Hartlage <kira.hartlage@gmail.com>
2017-03-28 14:06:41 -0400

    finished process JSON video and adding github basics folder

--- AJAX_Basics_Project_Files/stage2/video5/js/widget.js
+++ AJAX_Basics_Project_Files/stage2/video5/js/widget.js
@@ -1,8 +1,26 @@
 var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data/employees.json');
+console.log("hello there");
 xhr.onreadystatechange = function () {
+  console.log(xhr.readyState);
+  console.log(xhr.status);
   if (xhr.readyState === 4) {
     var employees = JSON.parse(xhr.responseText);
+    console.log("hi there");
+    var statusHTML = '<ul class="bulleted">';
+    for (var i = 0; i < employees.length; i+=1) {
+        if (employees[i].inoffice === true) {
+            statusHTML += '<li class="in">';
+            console.log("made it to inoffice");
+        }
+        else {
+            statusHTML += '<li class="out">';
+        }
+        statusHTML += employees[i].name;
+        statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
    }
 };
-xhr.open('GET', 'data/employees.json');
 xhr.send();
