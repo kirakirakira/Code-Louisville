47a9f243d65b1542d30ddc1784fae69ca921696c
Kira Hartlage <kira.hartlage@gmail.com>
2017-03-29 08:44:02 -0400

    finished stage 2 code challenge

--- AJAX_Basics_Project_Files/stage2/video6/js/widget.js
+++ AJAX_Basics_Project_Files/stage2/video6/js/widget.js
@@ -1,4 +1,7 @@
+// Employee In or Out Status
+
 var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data/employees.json');
 xhr.onreadystatechange = function () {
   if(xhr.readyState === 4 && xhr.status === 200) {
     var employees = JSON.parse(xhr.responseText);
@@ -16,5 +19,26 @@ xhr.onreadystatechange = function () {
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+// Room Availability Status
+var xhr2 = new XMLHttpRequest();
+xhr2.open('GET', 'data/rooms.json');
+xhr2.onreadystatechange = function () {
+  if(xhr2.readyState === 4 && xhr2.status === 200) {
+    var rooms = JSON.parse(xhr2.responseText);
+    var statusHTML2 = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML2 += '<li class="empty">';
+      } else {
+        statusHTML2 += '<li class="full">';
+      }
+      statusHTML2 += rooms[i].room;
+      statusHTML2 += '</li>';
+    }
+    statusHTML2 += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML2;
+  }
+};
+xhr2.send();
