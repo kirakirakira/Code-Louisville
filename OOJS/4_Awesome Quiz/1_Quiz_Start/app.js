e3af1e6ff39986c7988aa698d4e4c87fb03e5c62
Kira Hartlage <kira.hartlage@gmail.com>
2017-06-10 16:15:36 -0400

    finished awesome quiz

---
+++ OOJS/4_Awesome Quiz/1_Quiz_Start/app.js
@@ -0,0 +1,8 @@
+var questions = [
+  new Question("Who was the first president of the United States?", ["George Washington", "Donald Trump"], "George Washington"),
+  new Question("What is Mo's middle name?", ["Strip", "Danger"], "Danger")
+];
+
+var quiz = new Quiz(questions);
+
+QuizUI.displayNext();
