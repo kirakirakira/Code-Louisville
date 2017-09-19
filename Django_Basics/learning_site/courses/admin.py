55bb931bf79acd563ca1da3848cf6515e973b35a
Kira Hartlage <kira.hartlage@gmail.com>
2017-09-19 15:12:15 -0400

    finished stackedinline form

--- Django_Basics/learning_site/courses/admin.py
+++ Django_Basics/learning_site/courses/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
 from .models import Course
+from .models import Step
 
-admin.site.register(Course)
+
+class StepInline(admin.StackedInline):
+    model = Step
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [StepInline,]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Step)
