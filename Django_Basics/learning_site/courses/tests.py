52dca35a1411b58eed9a120a7fe9c79e13c031a7
Kira Hartlage <kira.hartlage@gmail.com>
2017-09-21 10:00:19 -0400

    all the way to the last challenge

--- Django_Basics/learning_site/courses/tests.py
+++ Django_Basics/learning_site/courses/tests.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
@@ -15,11 +16,55 @@ class CourseModelTests(TestCase):
         self.assertLess(course.created_at, now)
 
 class StepModelTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Regular Expressions",
+            description="Learn to write tests in Python"
+        )
+
     def test_step_creation(self):
         step = Step.objects.create(
             title="Introduction to regex",
             description="Explore the key details of regex",
             content="Symbols are super crazy",
             order=0,
-            course=Course.objects.create(),
+            course=self.course
         )
+        self.assertIn(step, self.course.step_set.all())
+
+class CourseViewsTests(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Testing",
+            description="Learn to write tests in Python"
+        )
+        self.course2 = Course.objects.create(
+            title="New Course",
+            description="A new course"
+        )
+        self.step = Step.objects.create(
+            title="Introduction to Doctests",
+            description="Learn to write tests in your docstrings.",
+            course=self.course
+        )
+
+    def test_course_list_view(self):
+        resp = self.client.get(reverse('courses:list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(self.course, resp.context['courses'])
+        self.assertIn(self.course2, resp.context['courses'])
+        self.assertTemplateUsed(resp, 'courses/course_list.html')
+        self.assertContains(resp, self.course.title)
+
+    def test_course_detail_view(self):
+        resp = self.client.get(reverse('courses:detail',
+                                        kwargs={'pk': self.course.pk}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(self.course, resp.context['course'])
+
+    def test_step_detail(self):
+        resp = self.client.get(reverse('courses:step', kwargs={
+                    'course_pk': self.course.pk,
+                    'step_pk': self.step.pk}))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(self.step, resp.context['step'])
