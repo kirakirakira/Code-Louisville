14dfd340d3dd3df8cff690a69ebe39788040105b
Kira Hartlage <kira.hartlage@gmail.com>
2017-10-03 09:39:11 -0400

    onto model forms

--- Django_Forms/Django_Forms/S2V3/learning_site/courses/models.py
+++ Django_Forms/Django_Forms/S2V3/learning_site/courses/models.py
@@ -6,7 +6,7 @@ class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.title
 
@@ -16,18 +16,18 @@ class Step(models.Model):
     description = models.TextField()
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course)
-    
+
     class Meta:
         abstract = True
         ordering = ['order',]
-    
+
     def __str__(self):
         return self.title
 
 
 class Text(Step):
     content = models.TextField(blank=True, default='')
-    
+
     def get_absolute_url(self):
         return reverse('courses:text', kwargs={
                 'course_pk': self.course_id,
@@ -37,7 +37,7 @@ class Text(Step):
 
 class Quiz(Step):
     total_questions = models.IntegerField(default=4)
-    
+
     class Meta:
         verbose_name_plural = "Quizzes"
 
@@ -46,3 +46,39 @@ class Quiz(Step):
                 'course_pk': self.course_id,
                 'step_pk': self.id
             })
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz)
+    order = models.IntegerField(default=0)
+    prompt = models.TextField()
+
+    class Meta:
+        ordering = ['order',]
+
+    def get_absolute_url(self):
+        return self.quiz.get_absolute_url()
+
+    def __str__(self):
+        return self.prompt
+
+
+class MultipleChoiceQuestion(Question):
+    shuffle_answers = models.BooleanField(default=False)
+
+
+class TrueFalseQuestion(Question):
+    pass
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    order = models.IntegerField(default=0)
+    text = models.CharField(max_length=255)
+    correct = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['order',]
+
+    def __str__(self):
+        return self.text
