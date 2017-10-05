319df8fc3819cfceebdb5f16193d74cd349e1388
Kira Hartlage <kira.hartlage@gmail.com>
2017-10-05 08:39:00 -0400

    forms forms forms

---
+++ Django_Forms/Django_Forms/S2V3/learning_site/courses/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from . import models
+
+
+class QuizForm(forms.ModelForm):
+    class Meta:
+        model = models.Quiz
+        fields = [
+            'title',
+            'description',
+            'order',
+            'total_questions',
+        ]
+
+
+class TrueFalseQuestion(forms.ModelForm):
+    class Meta:
+        model = models.TrueFalseQuestion
+        fields = ['order', 'prompt']
+
+
+class MultipleChoiceQuestion(forms.ModelForm):
+    class Meta:
+        model = models.MultipleChoiceQuestion
+        fields = [
+            'order',
+            'prompt',
+            'shuffle_answers',
+        ]
