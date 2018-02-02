9e58289451c1d8e588ba5826239602d953ee2a0e
Kira Hartlage <kira.hartlage@gmail.com>
2018-02-02 15:54:35 -0500

    working on rest api

---
+++ FSJSprojectplan.js
@@ -0,0 +1,12 @@
+{_id: blah,
+  Monday: {
+    Breakfast: [meal, {protein: 1, fruit: 1}],
+    Lunch: [meal, {protein: 1}],
+    Dinner: [meal, {protein: 1}]
+  },
+  Tuesday: {
+    Breakfast: [meal, {protein: 1, fruit: 1}],
+    Lunch: [meal, {protein: 1}],
+    Dinner: [meal, {protein: 1}]
+  },
+}
--- REST_API_Express/qa-rest-api/app.js
+++ REST_API_Express/qa-rest-api/app.js
@@ -4,11 +4,33 @@ var express = require('express');
 var app = express();
 var routes = require("./routes");
 
-var jsonParser = require('body-parser').json;
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
-app.use(jsonParser());
+app.use(logger("dev"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/questions", routes);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
 
 var port = process.env.PORT || 3000;
 
