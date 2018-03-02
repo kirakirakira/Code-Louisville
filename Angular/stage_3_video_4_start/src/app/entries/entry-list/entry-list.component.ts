cf5630c835c91266cd198e77f6b98c49d6a336c1
Kira Hartlage <kira.hartlage@gmail.com>
2018-03-02 10:05:17 -0500

    adding angular files

---
+++ Angular/stage_3_video_4_start/src/app/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-entry-list',
+    templateUrl: 'entry-list.component.html',
+    styleUrls: ['entry-list.component.css']
+})
+
+export class EntryListComponent {
+    constructor() {
+        
+    }
+}
