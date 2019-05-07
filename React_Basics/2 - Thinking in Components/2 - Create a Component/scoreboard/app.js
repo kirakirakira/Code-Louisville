4cc6795f50035c8b5d4b0e749b872a5f549cf2ba
Kira Hartlage <kira.hartlage@gmail.com>
2019-05-06 20:29:53 -0400

    halfway through state

--- React_Basics/2 - Thinking in Components/2 - Create a Component/scoreboard/app.js
+++ React_Basics/2 - Thinking in Components/2 - Create a Component/scoreboard/app.js
@@ -34,19 +34,38 @@ const Player = (props) => {
       <span className="player-name">
         { props.name }
       </span>
-      <Counter score={ props.score } />
+      <Counter />
     </div>
   );
 };
 
-const Counter = (props) => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{ props.score }</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
+class Counter extends React.Component {
+
+  state = {
+    score: 0
+  };
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+  };
+
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <span className="counter-score">{ this.state.score }</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    );
+  }
 };
 
 const App = (props) => {
@@ -58,7 +77,6 @@ const App = (props) => {
       { props.initialPlayers.map(player =>
         <Player
           name={player.name}
-          score={player.score}
           key={player.id.toString()}
         />)
       }
