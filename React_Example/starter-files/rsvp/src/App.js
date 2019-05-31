9e2224ad2c261e4210dbabc6f8c15f1064e14f0a
Kira Hartlage <kira.hartlage@gmail.com>
2019-05-31 13:55:14 -0400

    updated rsvp app

--- React_Example/starter-files/rsvp/src/App.js
+++ React_Example/starter-files/rsvp/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Counter from './Counter';
 import GuestList from './GuestList';
 
 class App extends Component {
 
   state = {
     isFiltered: false,
+    pendingGuest: "",
     guests: [
       {
         name: 'Treasure',
@@ -43,6 +45,15 @@ class App extends Component {
     this.toggleGuestPropertyAt('isConfirmed', index);
   }
 
+  removeGuestAt = index => {
+    this.setState({
+      guests: [
+        ...this.state.guests.slice(0, index),
+        ...this.state.guests.slice(index+1)
+      ]
+    })
+  }
+
   toggleEditingAt = index => {
     this.toggleGuestPropertyAt('isEditing', index);
   }
@@ -65,28 +76,56 @@ class App extends Component {
     this.setState({ isFiltered: !this.state.isFiltered })
   }
 
+  handleNameInput = e =>
+    this.setState({
+      pendingGuest: e.target.value
+    })
+
+  newGuestSubmitHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      guests: [
+        {
+          name: this.state.pendingGuest,
+          isConfirmed: false,
+          isEditing: false
+        },
+        ...this.state.guests
+      ],
+      pendingGuest: ''
+    })
+  }
+
   getTotalInvited = () => this.state.guests.length;
 
   getConfirmedGuests() {
     return this.state.guests.filter((guest) => {
-      return this.guest.isConfirmed
-    });
+      return guest.isConfirmed
+    }).length;
   }
 
   getUnconfirmedGuests() {
     return this.state.guests.filter((guest) => {
-      return !this.guest.isConfirmed
-    });
+      return !guest.isConfirmed
+    }).length;
   }
 
   render() {
+    const totalInvited = this.getTotalInvited();
+    const numberAttending = this.getConfirmedGuests();
+    const numberUnconfirmed = this.getUnconfirmedGuests();
     return (
       <div className="App">
         <header>
           <h1>RSVP</h1>
           <p>A Treehouse App</p>
-          <form>
-              <input type="text" value="Safia" placeholder="Invite Someone" />
+          <form onSubmit={this.newGuestSubmitHandler}>
+              <input
+                type="text"
+                value={this.state.pendingGuest}
+                placeholder="Invite Someone"
+                onChange={this.handleNameInput}
+              />
               <button type="submit" name="submit" value="submit">Submit</button>
           </form>
         </header>
@@ -101,28 +140,19 @@ class App extends Component {
               /> Hide those who haven't responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+          <Counter
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed}
+          />
           <GuestList
             guests={this.state.guests}
             toggleConfirmationAt={this.toggleConfirmationAt}
             toggleEditingAt={this.toggleEditingAt}
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
+            removeGuestAt={this.removeGuestAt}
+            pendingGuest={this.state.pendingGuest}
           />
         </div>
       </div>
